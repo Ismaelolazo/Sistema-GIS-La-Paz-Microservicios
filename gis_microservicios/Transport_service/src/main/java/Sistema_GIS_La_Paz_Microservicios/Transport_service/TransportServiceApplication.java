db2483d088823c9fff9ccdda6ed2c1ac5f5b4921
Joaquin Alfredo Mancilla Aguilar <125755724+JMancillaA@users.noreply.github.com>
2025-03-07 20:15:42 -0400

    Update TransportServiceApplication.java

--- gis_microservicios/Transport_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Transport_service/TransportServiceApplication.java
+++ gis_microservicios/Transport_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Transport_service/TransportServiceApplication.java
@@ -2,8 +2,13 @@ package Sistema_GIS_La_Paz_Microservicios.Transport_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "Sistema_GIS_La_Paz_Microservicios.Transport_service",
+    "Sistema_GIS_La_Paz_Microservicios.controller"
+})
 public class TransportServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(TransportServiceApplication.class, args);
