ac4e8bd0dba83e288ab004e6888e3b3dd2c31cff
Ruddy <91032302+Fesur@users.noreply.github.com>
2025-03-09 04:58:35 -0400

    Modificacion de Usuario service
    Modificando archivos y codigo para que de funcionamiento a los comandos de postman GET,POST,PUT,DELETE

--- gis_microservicios/Usuario_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Usuario_service/controller/UsuarioController.java
+++ gis_microservicios/Usuario_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Usuario_service/controller/UsuarioController.java
@@ -1,23 +1,67 @@
+
 package Sistema_GIS_La_Paz_Microservicios.Usuario_service.controller;
-import Sistema_GIS_La_Paz_Microservicios.Usuario_service.response.UsuarioResponse;
-import Sistema_GIS_La_Paz_Microservicios.Usuario_service.service.UsuarioService;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import Sistema_GIS_La_Paz_Microservicios.Usuario_service.entity.Usuario;
+import Sistema_GIS_La_Paz_Microservicios.Usuario_service.repository.UsuarioRepo;
 
 @RestController
 public class UsuarioController {
-
-    @Autowired
-    private UsuarioService UsuarioService;
-
-    @GetMapping("/Usuarios/{id}")
-    private ResponseEntity<UsuarioResponse> getUsuarioDetails(@PathVariable("id") int id) {
-        UsuarioResponse Usuario = UsuarioService.getUsuarioById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(Usuario);
-    }
+	
+	@Autowired
+	UsuarioRepo repo;
+	//get all the usuarios 
+	//localhost:8080/usuarios
+	@GetMapping("/Usuario")
+	public List<Usuario> getAllusuarios(){
+		 List<Usuario> usuarios = repo.findAll();
+		  return usuarios;
+	}
+	
+	//localhost:8080/usuarios/1
+	@GetMapping("/Usuario/{id}")
+	public Usuario getUsuario(@PathVariable int id) {
+		Usuario Usuario = repo.findById(id).get();
+		
+		return Usuario;
+		
+	}
+	
+	@PostMapping("/Usuario/add")
+	public ResponseEntity<Usuario> createUsuario(@RequestBody Usuario Usuario) {
+		Usuario newUsuario = repo.save(Usuario);
+		return ResponseEntity.status(HttpStatus.CREATED).body(newUsuario);
+		
+	}
+	
+	@PutMapping("/Usuario/update/{id}")
+	public Usuario updateusuarios(@PathVariable int id) {
+	   Usuario Usuario = repo.findById(id).get();
+	   Usuario.setName("test usuario update");
+	   Usuario.setEmail("test email update");
+	   Usuario.setPassword("test contra update");
+	   repo.save(Usuario);
+	   return Usuario;
+		
+	}
+	@DeleteMapping("/Usuario/delete/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void removeUsuario(@PathVariable int id) {
+		Usuario Usuario = repo.findById(id).get();
+		repo.delete(Usuario);
+	}
 
 }
--- gis_microservicios/Usuario_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Usuario_service/entity/Usuario.java
+++ gis_microservicios/Usuario_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Usuario_service/entity/Usuario.java
@@ -1,53 +1,70 @@
 package Sistema_GIS_La_Paz_Microservicios.Usuario_service.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "usuarios")
+@Table(name = "Usuarios")
 public class Usuario {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Id
     @Column(name = "ID_usuario")
     private int id;
+	@Column(name = "Nombre")
+	private String name;
+	@Column(name="Correo")
+	private String email;
+	@Column(name="Contra")
+	private String password;
+	
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Usuario(int id,String name, String email, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+	}
 
-    @Column(name = "Nombre")
-    private String name;
 
-    @Column(name = "Correo")
-    private String email;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password
+				+ "]";
+	}
+	
+	
 
-    @Column(name = "Contra")
-    private String password;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
