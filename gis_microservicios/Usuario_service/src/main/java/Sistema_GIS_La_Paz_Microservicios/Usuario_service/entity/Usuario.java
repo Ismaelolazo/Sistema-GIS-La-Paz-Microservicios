a8db0d02a0fa2d0954f44c894627be229f7e39a7
Ruddy <91032302+Fesur@users.noreply.github.com>
2025-03-09 05:04:28 -0400

    modificaciones y eliminando archivos innecesarios

--- gis_microservicios/Usuario_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Usuario_service/entity/Usuario.java
+++ gis_microservicios/Usuario_service/src/main/java/Sistema_GIS_La_Paz_Microservicios/Usuario_service/entity/Usuario.java
@@ -2,8 +2,6 @@ package Sistema_GIS_La_Paz_Microservicios.Usuario_service.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
