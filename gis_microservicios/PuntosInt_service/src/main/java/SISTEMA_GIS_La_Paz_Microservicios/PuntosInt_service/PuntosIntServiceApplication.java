6583aa49ea417a913562d173460e16027b2b398c
MarcoYeahBuddy <144861069+MarcoYeahBuddy@users.noreply.github.com>
2025-03-10 02:37:51 -0400

    initialzr PuntosInt

---
+++ gis_microservicios/PuntosInt_service/src/main/java/SISTEMA_GIS_La_Paz_Microservicios/PuntosInt_service/PuntosIntServiceApplication.java
@@ -0,0 +1,13 @@
+package SISTEMA_GIS_La_Paz_Microservicios.PuntosInt_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntosIntServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntosIntServiceApplication.class, args);
+	}
+
+}
